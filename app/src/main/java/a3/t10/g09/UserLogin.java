fae65c990e38a0d61c492fa50d619cd386c9d0c7
Kenton Lee <klee0078@uni.sydney.edu.au>
2025-10-14 13:48:00 +1100

    feat(UserLogin): initial method functionality. Added getUserData which retrieves user data from the JSON

--- app/src/main/java/a3/t10/g09/UserLogin.java
+++ app/src/main/java/a3/t10/g09/UserLogin.java
@@ -6,40 +6,71 @@ public class UserLogin {
 
     Scanner scanner = new Scanner(System.in);
 
+    // Retrieve info from JSON file
+    private String userData = "data/users.json";
+
+    public String getUserData() {
+        // TODO Reads the JSON file and returns its content , may need to change type
+        return "";
+    }
+
     // Methods that interact with the JSON file 
     public boolean isValidUsername(String username) {
         // Checks if username is existing inside the json file
+        // TODO check if username exists in JSON file
         return false;
     }
 
     public boolean isValidUser(String username, String password) {
-        // Checks if pw matches the hashed pw and username inside the json file
+        // Checks if user inputted password matches the usernames' hashed passwords inside the json file
+        // TODO check if password matches the username in JSON file
         return false;
     }
 
     // Feedback messages
     public String getSuccessfulLoginMessage() {
-        return "";
+        return "User Login Successful! Directing to Dashboard...";
     }
 
     public String getUnsuccessfulUsername() {
-        return "";
+        return "ID key not found. Please check your credentials and try again.";
     }
 
     public String getUnsuccessfulPassword() {
-        return "";
+        return "Incorrect password. Please try again.";
     }
 
     // User input methods
     public String inputIDKey(){
-        return "";
+        System.out.println("Please enter your ID key: ");
+        String IDkey = scanner.next();
+        return IDkey;
     }
 
     public String inputPassword(){
-        return "";
+        System.out.println("Please enter password: ");
+        String password = scanner.next();
+        return password;
     }
     public boolean authenticateUser(String username, String password) {
         // Authenticate user against stored credentials
+
+        String data = this.getUserData(); 
+        String inputUsername = this.inputIDKey();
+        String inputPassword = this.inputPassword();
+
+        if(!this.isValidUsername(inputUsername)){
+            this.getUnsuccessfulUsername();
+            return false;
+        }
+        else if(!this.isValidUser(inputUsername, inputPassword)){
+            this.getUnsuccessfulPassword();
+            return false;
+        }
+        else{
+            this.getSuccessfulLoginMessage();
+            return true;
+        }
         return false;
     }
 }
