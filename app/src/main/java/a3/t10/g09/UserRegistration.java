cdb5ae98a88c8f3e772e41adb150d51ced76dd14
Kenton Lee <klee0078@uni.sydney.edu.au>
2025-10-14 15:21:22 +1100

    feat(hashPassword): method to securely hash the password

--- app/src/main/java/a3/t10/g09/UserRegistration.java
+++ app/src/main/java/a3/t10/g09/UserRegistration.java
@@ -2,5 +2,12 @@ package a3.t10.g09;
 
 public class UserRegistration {
     // Placeholder for user registration logic
-    
+
+    // Method to hash passwords using BCrypt
+    public String hashPassword(String password){
+        String salt = BCrypt.gensalt(12);
+        String hashedPassword = BCrypt.hashpw(password, salt);
+        
+        return hashedPassword;
+    }
 }
