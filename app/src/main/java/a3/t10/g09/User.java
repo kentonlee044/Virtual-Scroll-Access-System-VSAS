18848599db0dafd45b1b6a3175595832e17fc346
Kenton Lee <klee0078@uni.sydney.edu.au>
2025-10-14 14:58:38 +1100

    refactor(UserLogin): Includes role access and refactored to handle JSON correctly

---
+++ app/src/main/java/a3/t10/g09/User.java
@@ -0,0 +1,34 @@
+package a3.t10.g09;
+
+public class User {
+    private String idkey;
+    private String username;
+    private String email;
+    private String fullname;
+    private String phoneNumber;
+    private String role;
+    private String password; // This should be the hashed password
+
+    // Getters
+    public String getIdkey() { 
+        return idkey; 
+    }
+    public String getUsername() { 
+        return username; 
+    }
+    public String getEmail() { 
+        return email; 
+    }
+    public String getFullname() { 
+        return fullname; 
+    }
+    public String getPhoneNumber() { 
+        return phoneNumber; 
+    }
+    public String getRole() { 
+        return role; 
+    }
+    public String getPassword() { 
+        return password; 
+    }
+}
