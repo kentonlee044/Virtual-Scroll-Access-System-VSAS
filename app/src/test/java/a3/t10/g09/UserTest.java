5c624c53919c9f5d1a1cf577a20c2cd4262731d8
adai0325 <adai0325@uni.sydney.edu.au>
2025-10-15 20:01:55 +1100

    chore(registration): fix imports in tests after moving UserRegistration package

--- app/src/test/java/a3/t10/g09/UserTest.java
+++ app/src/test/java/a3/t10/g09/UserTest.java
@@ -6,51 +6,53 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mindrot.jbcrypt.BCrypt;
 
-class UserTest{
+import a3.t10.g09.Registration.UserRegistration;
+
+class UserTest {
 
     private User user;
-    
+
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
 
         UserRegistration userReg1 = new UserRegistration();
         String password = userReg1.hashPassword("SOFT2412");
 
-        user = new User("1", "Thomas", "thomas@gmail.com", "Thomas Shelby", "1234567890", "admin", password); 
+        user = new User("1", "Thomas", "thomas@gmail.com", "Thomas Shelby", "1234567890", "admin", password);
     }
 
-    @Test 
-    public void getIdkeyTest(){
+    @Test
+    public void getIdkeyTest() {
         assertEquals("1", user.getIdkey());
     }
 
-    @Test 
-    public void getUsernameTest(){
+    @Test
+    public void getUsernameTest() {
         assertEquals("Thomas", user.getUsername());
     }
 
     @Test
-    public void getEmailTest(){
+    public void getEmailTest() {
         assertEquals("thomas@gmail.com", user.getEmail());
     }
 
     @Test
-    public void getFullnameTest(){
+    public void getFullnameTest() {
         assertEquals("Thomas Shelby", user.getFullname());
     }
 
     @Test
-    public void getPhoneNumberTest(){
+    public void getPhoneNumberTest() {
         assertEquals("1234567890", user.getPhoneNumber());
     }
 
     @Test
-    public void getRoleTest(){
+    public void getRoleTest() {
         assertEquals("admin", user.getRole());
     }
 
     @Test
-    public void getPasswordTest(){
+    public void getPasswordTest() {
         String password = "SOFT2412";
         boolean hashedPassword = BCrypt.checkpw(password, user.getPassword());
         assertTrue(hashedPassword);
