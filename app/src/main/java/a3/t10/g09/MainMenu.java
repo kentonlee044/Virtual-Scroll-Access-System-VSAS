166a5f947fe6517572cc98b0bb85827c352445dd
adai0325 <adai0325@uni.sydney.edu.au>
2025-10-16 10:01:13 +1100

    feat(login): split CLI UI from authentication logic
    Keep UserLogin focused on data loading and credential checks
    Add LoginCli to provide styled terminal login experience
    Update MainMenu to launch the new CLI flow

--- app/src/main/java/a3/t10/g09/MainMenu.java
+++ app/src/main/java/a3/t10/g09/MainMenu.java
@@ -1,6 +1,7 @@
 package a3.t10.g09;
 
 import java.util.Scanner;
+import a3.t10.g09.Login.LoginCli;
 import a3.t10.g09.Registration.RegisterCli;
 
 public class MainMenu {
@@ -23,32 +24,21 @@ public class MainMenu {
                 }
 
                 switch (choice) {
-                    case 1: { // Log in
-                        UserLogin login = new UserLogin();
-                        UserList list = login.getUserData();
-                        if (list == null) {
-                            System.out.println("No user data found.\n");
-                            break;
-                        }
-                        String id = login.inputIDKey();
-                        String pw = login.inputPassword();
-                        login.authenticateUser(list, id, pw);
-                        break;
+                    case 1 -> {
+                        new LoginCli().run();
                     }
-                    case 2: { // Register
+                    case 2 -> {
                         RegisterCli.main(new String[0]);
-                        break;
                     }
-                    case 3: { // Guest
+                    case 3 -> {
                         System.out.println("Continuing as Guest...\n");
-                        // TODO: launch guest flow if needed
-                        break;
+                        // TODO: guest flow
                     }
-                    case 0:
+                    case 0 -> {
                         System.out.println("Goodbye.");
                         return;
-                    default:
-                        System.out.println("Please choose 0, 1, 2, or 3.\n");
+                    }
+                    default -> System.out.println("Please choose 0, 1, 2, or 3.\n");
                 }
             }
         }
