aa887c72604201a4e95a640d2a5bf1219d1c143f
khof0998 <khof0998@uni.sydney.edu.au>
2025-10-19 23:02:51 +1100

    test(a3/t10/g09): added tests for Client, CommandMenu, ProfileUpdateController and ProfileUpdateHandler

---
+++ app/src/test/java/a3/t10/g09/ClientTest.java
@@ -0,0 +1,22 @@
+package a3.t10.g09;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ClientTest {
+    Client client;
+    @BeforeEach
+    public void setUp() {
+        client = new Client();
+    }
+    @Test
+    public void testGetters() {
+        assertEquals("N/A", client.getUsername());
+        assertEquals(ClientStatus.ANONYMOUS, client.getUserType());
+        client.getAvailableCommands();
+        client.getCurrentUser();
+    }
+}
---
+++ app/src/test/java/a3/t10/g09/CommandMenuTest.java
@@ -0,0 +1,4 @@
+package a3.t10.g09;
+import org.junit.jupiter.api.Test;
+public class CommandMenuTest {
+}
